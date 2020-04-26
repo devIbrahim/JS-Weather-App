fd3e28bdc102b0679d18afacf00d6a6f6451376e
Syed Anees Ibrahim <50572067+devIbrahim@users.noreply.github.com>
2020-04-26 16:26:42 +0530

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = 'HPdBV5WtmhZjig4OBz5kXc3epe0ZSRWJ';
+
+
+//GET CITY INFO
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+   return data[0];
+
+};
+
+//GET WEATHER INFO
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]
+
+};
